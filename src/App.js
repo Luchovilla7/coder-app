23b41cf52ef69ec90fa10af61566bec05b6ce015
Luciano Villalba <luchovillalba7@gmail.com>
2022-08-23 15:53:53 -0300

    navbar correction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <header>
-      <div className="header__content container">
-        <h3>
-          CANNABIG 🌱
-        </h3>
-        <nav>
-          <ul className="content__navbar">
-            <li><a href="">Home</a></li>
-            <li><a href="">Productos</a></li>
-            <li><a href="">Nosotros</a></li>
-            <li><a href="">Carrito</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <NavBar />
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const NavBar = () =>{
+    return(
+    <header>
+      <div className="header__content container">
+        <h3>
+          CANNABIG 🌱
+        </h3>
+        <nav>
+          <ul className="content__navbar">
+            <li><a href="">Home</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Carrito</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    );
+}
+
+export default NavBar;

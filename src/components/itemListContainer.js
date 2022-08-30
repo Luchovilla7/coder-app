ea5de2af2206929f4fb16cefe42d472223a9465c
Luciano Villalba <luchovillalba7@gmail.com>
2022-08-30 16:41:02 -0300

    count button

--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,8 +1,17 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({greeting}) => {
+
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return(
-        <h1>{greeting}</h1>
+        <>
+            <h1>{greeting}</h1>
+            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </>
     )
 }
 

52e14431d406b2175dcb0b15fd0422310d1c2421
Luciano Villalba <luchovillalba7@gmail.com>
2022-09-22 15:49:35 -0300

    correccion cartView

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,29 +2,34 @@ import React from "react";
 import { useCartContext } from "../cartcontext/CartContext";
 import { Link } from "react-router-dom";
 import ItemCart from "./ItemCart";
+import { useContext } from "react";
 
 
 const Cart = () =>{
-    const { cart, totalPrice } = useCartContext();
+    const { cart, totalPrice, clearCart } = useCartContext();
 
     if (cart.length === 0) {
         return (
-            <div className="cartEmpty">
-                <p>No hay productos en el carrito</p>
-                <Link to='/'>Hacer compra</Link>
+            <div className="cartDetail">
+                <div className="empty">
+                    <p>No hay productos en el carrito</p>
+                    <Link to='/'>Hacer compra</Link>
+                </div>
             </div>
         );
     }
 
-    return(
-        <>
-            {
-                cart.map(product => <ItemCart key={product.id} product={product} />)
-            }
-            <p>
-                total: ${totalPrice()}
-            </p>
-        </>
+    return (
+        <div className="cartDetail">
+            {cart.map((product) => (
+                    <ItemCart key={product.id} product={product} />
+            ))}
+            <div className="check">
+                <p>Total a pagar: ${totalPrice()}</p>
+                <button>Comprar ahora</button>
+                <button onClick={() => clearCart()}>Eliminar todo</button>
+            </div>
+        </div>
     );
 }
 
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,10 +4,10 @@ import { useCartContext } from "../cartcontext/CartContext";
 export const CartWidget = () => {
     const { totalProducts } = useCartContext();
     return(
-        <>
+        <div className="cart-cont">
             <i className="bi bi-cart3"></i>
             <span>{totalProducts() || ''}</span>
-        </>
+        </div>
     );
 }
 

323abf27d6ca71fd5ca3729d3d1fb825f47c27d8
Luciano Villalba <luchovillalba7@gmail.com>
2022-09-27 17:50:18 -0300

    firebase

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,8 @@
 import React, {useState, useEffect} from "react";
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import BounceLoader from 'react-spinners/BounceLoader';
-
-const products = [
-    { id: 1, price: 2250, image: "https://cannapp.com.co/wp-content/uploads/2021/03/0010s_0016_Aceite-DnSelva-30ml-Caja-Frontal.jpg", category:'aceites', title: "Aceite de Cannabis Standart 30ml"},
-    { id: 2, price: 2600, image: "https://cannapp.com.co/wp-content/uploads/2022/01/0002s_0000_KIF-Cannabis-Power-Skin-Control-50g-Front.jpg", category:'cremas', title: "Crema Humectante de Cannabis 60grs"},
-    { id: 3, price: 2550, image: "https://cannapp.com.co/wp-content/uploads/2020/08/pomada.jpg", category:'pomadas', title: "Pomada de Cannabis y Caléndula 60gr"},
-    { id: 4, price: 2500, image: "https://cannapp.com.co/wp-content/uploads/2021/08/Preparaci%C3%B3n-Magistral-57-Med-Gotas-30ml-Caja-Blanca-Front.jpg", category:'aceites', title: "Gotas de CBD aislado 30ml"},
-    { id: 5, price: 2200, image: "https://cannapp.com.co/wp-content/uploads/2022/01/0000s_0003_Tratamiento-Capilar-SativCare-250ml-front.jpg", category:'cremas', title: "Tratamiento capilar con Cannabis 250ml"},
-    { id: 6, price: 2800, image: "https://cannapp.com.co/wp-content/uploads/2020/08/Plantilla-Productos_0016s_0004_1.-CANNAHEALTH-OMG-copia.jpg", category:'cremas', title: "Gel para dolores musculares con Cannabis 100gr"},
-];
 
 const ItemListContainer = ({greeting}) => {
     const [data, setData] = useState([]);
@@ -20,20 +12,20 @@ const ItemListContainer = ({greeting}) => {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const getData = new Promise(resolve =>{
-            setTimeout(() => {
-                resolve(products);
-            });
-        });
-        if (categoriaId){
-            getData.then(res => setData(res.filter(aceite => aceite.category === categoriaId ))); 
+        const querydb = getFirestore();
+        const queryCollection = collection(querydb, 'products');
+         if (categoriaId){
+            const queryFilter = query(queryCollection, where('category', '==', categoriaId))
+            getDocs(queryFilter)
+            .then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))    
         } else {
-            getData.then(res => setData(res));
+            getDocs(queryCollection)
+            .then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))    
         }
         setLoading(true);
         setTimeout (() => {
         setLoading(false);
-        }, 2000);
+        });
 
     }, [categoriaId])
 
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDH6MO7RdjWltx7CZ4f_zZo6m14D0EpbF4",
+  authDomain: "coder-app-dcb3f.firebaseapp.com",
+  projectId: "coder-app-dcb3f",
+  storageBucket: "coder-app-dcb3f.appspot.com",
+  messagingSenderId: "708086233880",
+  appId: "1:708086233880:web:cbb1a9ddbc8c80d4bafc6d"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
